94f721d7731ef6582118bafc297a3314671c7a61
Lars Jørgen Solberg <supersolberg@gmail.com>
2013-06-23 10:50:11 +0200

    basic plugin

--- plugin/edowser.cpp
+++ plugin/edowser.cpp
@@ -9,32 +9,34 @@
 #include <QTimer>
 
 Edowser::Edowser(QWidget *parent)
-  : QX11EmbedContainer(parent) {  
-  qDebug() << "XID:" << this->winId();
+  : QWidget(parent) {  
+  
+  container = new QX11EmbedContainer(this);
+  container->setGeometry(0, 0, this->width(), this->height());
 
-  //there appears to be a bug in emacs that makes it use a tiny part of the window until it's resized
-  //to make matters worse, this has no effect until it's completely finsihed with its startup files
-  QTimer::singleShot(1000, this, SLOT(resizeNudge()));
-  QTimer::singleShot(5000, this, SLOT(resizeNudge()));
-  QTimer::singleShot(10000, this, SLOT(resizeNudge()));
-}  
-
-Edowser::~Edowser(){
-  process->waitForFinished();
-  delete process;
-}
-
-void Edowser::show() {
-  QX11EmbedContainer::show();
-
-  QString command = QString("emacs ");
-  command.append("--parent-id ");
-  command.append(QString::number(this->winId()));
+  qDebug() << "XID:" << container->winId();
+  QString command = QString("xterm ");
+  command.append("-into ");
+  command.append(QString::number(container->winId()));
   process = new QProcess(this);
   qDebug() << "editor command:" << command;
   process->start(command);
   qDebug() << "editor PID:" << process->pid();
   process->waitForStarted();
+
+
+  //there appears to be a bug in emacs that makes it use a tiny part of the window until it's resized
+  //to make matters worse, this has no effect until it's completely finsihed with its startup files
+  //QTimer::singleShot(1000, this, SLOT(resizeNudge()));
+  //QTimer::singleShot(5000, this, SLOT(resizeNudge()));
+  //QTimer::singleShot(10000, this, SLOT(resizeNudge()));
+
+}  
+
+Edowser::~Edowser(){
+  process->waitForFinished();
+  delete process;
+  delete container;
 }
 
 void Edowser::resizeNudge() {
@@ -42,11 +44,11 @@ void Edowser::resizeNudge() {
   const int height = this->height();
 
   qDebug() << "resizeNudge()";
-
+  
   QResizeEvent event1(QSize(width, height), QSize(width + 1, height + 1));
   QResizeEvent event2(QSize(width + 1, height + 1), QSize(width, height));
-  QCoreApplication::sendEvent(this, &event1);
-  QCoreApplication::sendEvent(this, &event2);
+  QCoreApplication::sendEvent(container, &event1);
+  QCoreApplication::sendEvent(container, &event2);
 }
 
 int main(int argc, char *argv[]){
@@ -62,3 +64,7 @@ int main(int argc, char *argv[]){
 
   return retval;
 }
+
+QTNPFACTORY_BEGIN("Edowser", "A plugin that lets you edit textareas with a proper editor.")
+QTNPCLASS(Edowser)
+QTNPFACTORY_END()
--- plugin/edowser.h
+++ plugin/edowser.h
@@ -1,12 +1,16 @@
 #ifndef __EDOWSER_H_
 #define __EDOWSER_H_
 
+#include <QtGui>
 #include <QWidget>
 #include <QX11EmbedContainer>
 #include <QProcess>
+#include <qtbrowserplugin.h>
 
-class Edowser : public QX11EmbedContainer{
+class Edowser : public QWidget {
 Q_OBJECT
+Q_CLASSINFO("MIME", "application/x-edowser:---:--")
+Q_CLASSINFO("ToSuperClass", "Edowser")
 public:
   Edowser(QWidget *parent = 0);
   virtual ~Edowser();
@@ -15,8 +19,7 @@ public slots:
   void resizeNudge();
 private:
   QProcess *process;
+  QX11EmbedContainer *container;
 };
-
-
 
 #endif //__EDOWSER_H_
