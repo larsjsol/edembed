61b5b552e0c1f75fbbfa9f1f0ea36b71f604bf11
Lars Jørgen Solberg <supersolberg@gmail.com>
2013-09-23 20:14:31 +0200

    fixed a stupid bug

--- update_version.sh
+++ update_version.sh
@@ -9,4 +9,4 @@ VER=$1
 
 perl -pi -e "s/^Version=.*/Version=$VER/" xpi/application.ini
 perl -pi -e "s/<em:version>.*<\/em:version>/<em:version>$VER<\/em:version>/" xpi/install.rdf
-perl -pi -e "s/\"version\": \"0.1\",/\"version\": \"$VER\",/" crx/manifest.json 
+perl -pi -e "s/\"version\": \".*\",/\"version\": \"$VER\",/" crx/manifest.json 
